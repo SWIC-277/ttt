e57552ed36c5c7503fc630c5c69d6e1d7ceb7f78
Manav Misra <manavm@visionwebsoft.com>
2022-10-28 11:57:14 -0500

    test: white_check_mark: updates the winner

--- src/hooks/reducer.test.js
+++ src/hooks/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from "./reducer";
 
-it("should only update the board with the current turn for 'null' position (only once)", () => {
+it("only updates the board with the current turn for 'null' position (only once)", () => {
   const state = {
     board: [null, null, null, null, null, null, null, null, null],
     turn: "X",
@@ -16,4 +16,20 @@ it("should only update the board with the current turn for 'null' position (only
 
   expect(newState.board[0]).toBe("X");
   expect(newState2.board[0]).toBe("X");
+});
+
+it("updates the winner after a winning move", () => {
+  const state = {
+    board: ["X", "O", "X", "O", "X", "O", null, null, null],
+    turn: "X",
+  };
+
+  const action = {
+    type: "made_move",
+    index: 6,
+  };
+
+  const newState = reducer(state, action);
+
+  expect(newState.winner).toBe("X");
 });
