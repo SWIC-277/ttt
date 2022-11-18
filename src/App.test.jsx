979d93aeb4967f8b7ca170190792575955c7791c
Manav Misra <manavm@visionwebsoft.com>
2022-11-18 07:53:52 -0600

    test: :white_check_mark: account for random

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -11,20 +11,28 @@ describe("App", () => {
     expect(squares).toHaveLength(9);
   });
 
-  it("alternates 'X' and 'O' whenever clicking squares", async () => {
+  it("alternates 'X' and 'O' whenever clicking empty squares", async () => {
     const user = userEvent.setup();
 
     render(<App />);
 
+    const h2 = screen.getByRole("heading", { level: 2 });
+    const isStartingWithX = h2.textContent.includes("X");
+
     const squares = screen.getAllByRole("button");
 
     await user.click(squares[0]);
+
     await user.click(squares[1]);
+
+    // Click same square again to make sure it doesn't change
+    // await user.click(squares[0]);
+
     await user.click(squares[2]);
 
-    expect(squares[0]).toHaveTextContent("X");
-    expect(squares[1]).toHaveTextContent("O");
-    expect(squares[2]).toHaveTextContent("X");
+    expect(squares[0]).toHaveTextContent(isStartingWithX ? "X" : "O");
+    expect(squares[1]).toHaveTextContent(isStartingWithX ? "O" : "X");
+    expect(squares[2]).toHaveTextContent(isStartingWithX ? "X" : "O");
   });
 
   it("displays 'X' as the winner", async () => {
@@ -34,11 +42,16 @@ describe("App", () => {
 
     render(<App />);
 
+    const h2 = screen.getByRole("heading", { level: 2 });
     const squares = screen.getAllByRole("button");
+
+    const isStartingWithX = h2.textContent.includes("X");
 
     await Promise.all(clicks.map((click) => user.click(squares[click])));
 
-    expect(screen.getByText("X Wins!")).toBeInTheDocument();
+    expect(
+      screen.getByText(isStartingWithX ? "X Wins!" : "O Wins!")
+    ).toBeInTheDocument();
   });
 
   it("resets the game whenever the 'reset game' button is clicked", async () => {
